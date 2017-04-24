6e35cbfb95d2468de381ce51ce5dbca8add1e112
s20151104703 <s20151104703@ciecimac17deimac.local>
2017-04-24 10:50:25 +0800

    Initial Commit

---
+++ qwa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  qwa
+//
+//  Created by s20151104703 on 17/4/24.
+//  Copyright © 2017年 s20151104703. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
