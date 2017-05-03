fb148502a7a09bd54b4e2f4758e56197470c649b
ciec20151104703 <s20151104703@ciecimac17deiMac.local>
2017-05-03 11:58:57 +0800

    jiafen4
    加上了回合

--- qwa/ViewController.swift
+++ qwa/ViewController.swift
@@ -12,11 +12,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var fen2: UILabel!
     @IBOutlet weak var fen1: UILabel!
+    @IBOutlet weak var fen3: UILabel!
+    @IBOutlet weak var fen4: UILabel!
     var a:Int = 0
     var b:Int = 0
+    var c:Int = 0
+    var d:Int = 0
     @IBAction func jiafen1(_ sender: Any) {
         a=a+1
         fen1.text=("\(a)");
+        if (a>10 && a>=2+b){
+            c=c+1;
+            fen3.text=("\(c)");
+            a=0;
+            b=0;
+            fen1.text=("\(a)");
+            fen2.text=("\(b)");
+        }
     }
     @IBAction func tui1(_ sender: Any) {
         a = Int(fen1.text!)!;
@@ -28,6 +40,15 @@ class ViewController: UIViewController {
     @IBAction func jiafen2(_ sender: Any) {
         b=b+1
         fen2.text=("\(b)");
+        if (b>10 && b>=2+a){
+            d=d+1;
+            fen4.text=("\(d)");
+            a=0;
+            b=0;
+            fen1.text=("\(a)");
+            fen2.text=("\(b)");
+        }
+
     }
     @IBAction func tui2(_ sender: Any) {
         b = Int(fen2.text!)!;
